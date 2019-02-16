1e348571104807a8cc0c46b278b816b477f25465
OswaldoPineda <carlos-1502010@hotmail.com>
2019-02-16 16:38:00 -0600

    #Aplicacion CRUD# Views template engine finished

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/users/signin',(req,res)=>{
-    res.render('/users/signin.hbs');
+    res.render('users/signin.hbs');
 });
 
 routes.get('/users/signup', (req,res)=>{
-    res.render('/users/signup.hbs');
+    res.render('users/signup.hbs');
 });
 
 
