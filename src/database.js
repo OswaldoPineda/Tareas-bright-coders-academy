1feae71c835c10448d57a53c213a04c011ca3079
OswaldoPineda <carlos-1502010@hotmail.com>
2019-02-17 19:12:03 -0600

    #Aplicacion-CRUD#  fix cluster

--- src/database.js
+++ src/database.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 // Conectamos mongoose y configuramos su funcionamiento basico
-mongoose.connect('mongodb://oswaldopineda:Carlos1234579@cluster0-shard-00-00-x4sqb.mongodb.net:27017,cluster0-shard-00-01-x4sqb.mongodb.net:27017,cluster0-shard-00-02-x4sqb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+mongoose.connect('mongodb://oswaldopineda:Carlos1234579@cluster0-shard-00-00-x4sqb.mongodb.net:27017,cluster0-shard-00-01-x4sqb.mongodb.net:27017,cluster0-shard-00-02-x4sqb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',)
 .then(db=> console.log('DB is connect...'))
 .catch(err=>console.log(err))
