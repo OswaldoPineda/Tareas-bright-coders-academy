c82b1a8b16191b2649b83db300e8c5a79010d2ba
OswaldoPineda <carlos-1502010@hotmail.com>
2019-02-17 19:07:55 -0600

    #Aplicacion-CRUD#  connect mongod local to cluster atlas

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 // Conectamos mongoose y configuramos su funcionamiento basico
-mongoose.connect('mongodb://localhost/contacts-db-app', {
+mongoose.connect('mongodb://oswaldopineda:Carlos1234579@cluster0-shard-00-00-x4sqb.mongodb.net:27017,cluster0-shard-00-01-x4sqb.mongodb.net:27017,cluster0-shard-00-02-x4sqb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
